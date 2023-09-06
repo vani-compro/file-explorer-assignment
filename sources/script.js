61f3fabdce89ef28afc161c80d10a59889cae683
vani-compro <vani.sharma@comprotechnologies.com>
2023-09-06 14:58:40 +0530

    changed ids to events mostly

--- sources/script.js
+++ sources/script.js
@@ -3,46 +3,62 @@ const file_structure = {
   folders: []
 }
 
-function buttonClicked(id, level){
-  console.log('btn clicked for: ' + id);
+function buttonClicked(event, level){
+  console.log('btn clicked for: ' + level);
 
-  //createUL
   const btn = event.target;
-  // console.log(event.target.children.contains(ul));
-  let ul = document.querySelector('#'+id+'-ul');
-  if (btn.contains(ul)){
-  }else{
-    ul = createUl(btn, id);
-  }
-
-  for(let i = 1; i <= level+1; i++){
-    ul.style.marginLeft = `${(i+1)}rem`
-  }
+  // console.log(btn);
+  // let ul;
+  // if(level){
+  //   ul = event.target.parentElement.parentElement.parentElement;
+  // }else{
+  //   ul = event.target.parentElement.parentElement;
+  // }
+  const ul = event.target.parentElement.parentElement;
+  console.log('called for btn: ' + btn);
   //arrow down
-  let arrow = document.querySelector('.arrows-'+id);
+  let arrow = document.querySelector('.arrows-'+level);
   arrow.classList.toggle('fa-angle-right');
   arrow.classList.toggle('fa-angle-down');
-  if(arrow.classList.contains('fa-angle-right')){
-    ul.style.display='none';
-    ul.style.visibility='hidden';
+  const childUl = ul.querySelector('ul');
+  if(!childUl){
+    console.log('inside button clicked if !childUl for btn: ' + btn);
   }else{
-    ul.style.display='block';
+    console.log('inside button clicked else !childUl for btn: '+btn);
+    if(arrow.classList.contains('fa-angle-right')){
+      childUl.style.display='none';
+      // childUl.style.visibility='hidden';
+    }else{
+      childUl.style.display='block';
+      // childUl.style.visibility='visible';
+    }
   }
 }
 
-function createUl(btn, id){
-  const ul = document.createElement('ul');
-  const ulId = id+'-ul';
-  ul.setAttribute('id', ulId);
-  const thisSection = document.querySelector('#btn-0-section');
-  thisSection.appendChild(ul);
-  return ul;
+function createUl(parentUl, level){
+  console.log('childul created for');
+    console.log(level);
+  const childUl = document.createElement('ul');
+  parentUl.appendChild(childUl);
+  console.log(childUl);
+  childUl.setAttribute('id','level'+(level+1));
+  childUl.style.marginLeft=`${level+1}rem`;
+  return childUl;
 }
 
-function addFile(location, level, id){
-  console.log(event);
-  event;
-  const ul = document.querySelector('#'+id+'-ul');
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function addFile(location, level){
+  let ul = null;
+  const parentUl = (event.target.parentElement.parentElement);
+  const childUl = parentUl.querySelector('ul');
+  if(!childUl){
+    console.log(('working'));
+    ul = createUl(parentUl, level);
+  }else{
+    ul=childUl;
+  }
+
   const li = document.createElement('li');
 
   const fileIcon = document.createElement("i");
@@ -60,6 +76,8 @@ function addFile(location, level, id){
   fileNameForm.style.display='inline';
   li.appendChild(fileIcon);
   li.appendChild(fileNameForm);
+  // let additionToId = Math.ceil(Math.random()*10000);
+  // li.setAttribute('id', '0-'+additionToId);
   ul.appendChild(li);
 
   //on submit
@@ -67,26 +85,40 @@ function addFile(location, level, id){
     event.preventDefault(); // Prevent the form from submitting
 
     const inputValue = textbox.value;
-    console.log(inputValue + ' file created under ul with id: ' + id);
+    console.log(inputValue + ' file created under ul with level: ' + level);
 
     let textnode = document.createTextNode(inputValue);
     li.appendChild(textnode);
     li.classList.add('level'+(level+1));
     fileNameForm.style.display = 'none';
-
-    location.files.push(inputValue);
+    console.log(location);
+    location['files'].push(`${inputValue}`);
     console.log(file_structure);
   });
 }
 
-function addFolder(location, level, id){
-  const ul = document.querySelector('#'+id+'-ul');
+function addFolder(location, level){
+  let ul = null;
+  console.log(event);
+  const parentUl = (event.target.parentElement.parentElement);
+  console.log(parentUl);
+  const childUl = parentUl.querySelector('ul');
+  console.log(childUl);
+  console.log(childUl);
+
+  if(!childUl){
+    console.log(('working2'));
+    ul = createUl(parentUl, level);
+  }else{
+    console.log('ul created in addfolder');
+    ul=childUl;
+  }
   const li = document.createElement('li');
 
   const arrow = document.createElement('i');
   arrow.classList.add('fa-solid');
   arrow.classList.add('fa-angle-right');
-  arrow.classList.add('arrows-btn-'+(level+1));
+  arrow.classList.add('arrows-'+(level+1));
   console.log(arrow.classList);
 
   const folderIcon = document.createElement("i");
@@ -105,6 +137,8 @@ function addFolder(location, level, id){
   fileNameForm.style.display='inline';
   li.appendChild(folderIcon);
   li.appendChild(fileNameForm);
+  // let additionToId = Math.ceil(Math.random()*10000);
+  // li.setAttribute('id', '0-'+additionToId);
   ul.appendChild(li);
 
   //on submit
@@ -116,13 +150,11 @@ function addFolder(location, level, id){
     folderIcon2.classList.add('fa-folder');
 
     const inputValue = textbox.value;
-    console.log(inputValue + ' folder created under ul with id: ' + id);
+    console.log(inputValue + ' folder created under ul with level: ' + level);
 
     const folderListBtn = document.createElement('button');
-    const newBtnId = 'btn-'+(level+1);
-    folderListBtn.setAttribute('id', newBtnId);
     folderListBtn.classList.add('level'+(level+1));
-    console.log(folderListBtn.getAttribute('id'));
+    // console.log(folderListBtn.getAttribute('id'));
 
     let textnode = document.createTextNode(inputValue);
 
@@ -142,34 +174,46 @@ function addFolder(location, level, id){
     li.appendChild(createFileIcon);
     li.appendChild(createFolderIcon);
 
-    folderListBtn.addEventListener('click', buttonClicked.bind(null, newBtnId, level+1));
+    folderListBtn.addEventListener('click', function(e){
+      buttonClicked(e, level+1);
+    });
 
     fileNameForm.style.display = 'none';
     folderIcon.style.display='none';
 
 
     let obj = {[`${inputValue}`] : {
-      'files':[],
-      'folders':[]
+      files:[],
+      folders:[]
      }};
     location["folders"].push(obj);
 
-    createFileIcon.addEventListener('click', function(){
-      const loc = location['files'];
+    createFileIcon.addEventListener('click', function(e){
+      const loc = location;
       console.log(loc);
-      addFile(loc, level+1, 'btn-'+(level+1));
+      addFile(loc, level+1);
+
+      // for(let i = 0; i<=10000; i++){
+      //   if (location['folders'][i][`${inputValue}`]){
+      //     const loc = location['folders'][i][`${inputValue}`];
+      //     console.log(location['folders'][i]);
+      //     addFile(loc, level+1);
+      //     break;
+      //   }
+      // }
     });
 
-    createFolderIcon.addEventListener('click', function(){
+    createFolderIcon.addEventListener('click', function(e){
       for(let i = 0; i<=10000; i++){
         if (location['folders'][i][`${inputValue}`]){
           const loc = location['folders'][i][`${inputValue}`];
-          console.log(loc);
-          addFolder(loc, level+1, 'btn-'+(level+1));
+          console.log(location['folders'][i]);
+          addFolder(loc, level+1);
           break;
         }
       }
     });
+
 
     console.log(file_structure);
     console.log(file_structure['folders'][0][1]);
